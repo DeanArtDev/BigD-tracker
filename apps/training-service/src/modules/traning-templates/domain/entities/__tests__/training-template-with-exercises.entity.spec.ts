2fde7578cd9f0ea919c550409f302d7d3ea147dd
Dmitriy Korobchenko <deankorobchenko@gmail.com>
2025-07-10 19:32:26 +0700

    Remove unused test and fix training-service paths

---
+++ apps/training-service/src/modules/traning-templates/domain/entities/__tests__/training-template-with-exercises.entity.spec.ts
@@ -0,0 +1,15 @@
+import { TrainingType, ExerciseType } from '@big-d/api-contracts';
+import { TrainingTemplateWithExercisesEntity } from '../training-template-with-exercises.entity';
+import { ExerciseWithRepetitionsEntity } from '../../../../exercises/domain';
+import { RepetitionEntity } from '../../../../repetitions/domain/repetition.entity';
+
+describe('TrainingTemplateWithExercisesEntity', () => {
+  it('sets exercises', () => {
+    const template = TrainingTemplateWithExercisesEntity.create({ name: 'T', type: TrainingType.LIGHT });
+    const exercise = ExerciseWithRepetitionsEntity.create({ name: 'Push', type: ExerciseType.AEROBIC, position: 0 });
+    const rep = RepetitionEntity.create({ exerciseId: exercise.id, position: 0, targetCount: 10, targetWeight: '10', targetBreak: 5 });
+    exercise.setRepetitions([rep]);
+    template.setExercises([exercise.assignToTemplate({ trainingTemplateId: template.id })]);
+    expect(template.exercises.length).toBe(1);
+  });
+});
