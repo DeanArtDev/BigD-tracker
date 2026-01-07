813d988c3927a08bc2d7039a75094946d982a3a1
Dean Korobchenko <deankorobchenko@gmail.com>
2026-01-07 18:34:27 +0700

    chore: add business condition

--- apps/goal-service/src/modules/tasks/application/use-cases/replace-task/replace-task.use-case.ts
+++ apps/goal-service/src/modules/tasks/application/use-cases/replace-task/replace-task.use-case.ts
@@ -4,17 +4,22 @@ import { TasksViewMapper } from '@/modules/tasks/application/dto/task.view-mappe
 import { Database } from '@/modules/tasks/infrastructure/database.interface';
 import { databaseToken } from '@big-d/database';
 import { Inject, Injectable } from '@nestjs/common';
-import { ReplaceTaskInput, TaskService } from '../../services';
+import { ReplaceTaskInput, TaskCheckerService, TaskService } from '../../services';
 
 @Injectable()
 class ReplaceTaskUseCase {
   constructor(
     private readonly taskServices: TaskService,
+    private readonly taskCheckerService: TaskCheckerService,
     @Inject(databaseToken.CONNECTION) private readonly db: Database<DB>,
   ) {}
 
   async execute(input: ReplaceTaskInput): Promise<TaskView> {
     return this.db.runTransaction(async (trx) => {
+      await this.taskCheckerService.ensureTaskExists(
+        { taskId: input.id, userId: input.userId },
+        { trx },
+      );
       const task = await this.taskServices.replaceTask(input, trx);
       return TasksViewMapper.fromAggregateToView(task);
     });
