6b6cddac85bbe42309bbc48d9a3b86b75bf75b9d
Dean Korobchenko <deankorobchenko@gmail.com>
2026-01-09 10:32:54 +0700

    chore: fix imports

--- apps/goal-service/src/modules/tasks/application/services/task.service.ts
+++ apps/goal-service/src/modules/tasks/application/services/task.service.ts
@@ -1,6 +1,6 @@
 import { DB } from '@/infrastructure/types';
 import { TasksWriteRepository } from '@/modules/tasks/application/ports';
-import { GroupCheckerService } from '@/modules/tasks/application/services/group-checker.service';
+import { GroupCheckerService } from '@/modules/tasks/application/services';
 import { Task, TaskFactory } from '@/modules/tasks/domain';
 import { TasksToken } from '@/modules/tasks/tokens';
 import { Inject, Injectable } from '@nestjs/common';
