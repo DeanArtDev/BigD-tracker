877533a28edd57d3ad284115a79ed9efedad6ac2
Dean Korobchenko <deankorobchenko@gmail.com>
2026-01-09 14:50:06 +0700

    refactor: optimize getInboxByUserId

--- apps/goal-service/src/modules/tasks/infrastructure/persistence/kysely/repositories/read/helpers/get-inbox-by-user-id.ts
+++ apps/goal-service/src/modules/tasks/infrastructure/persistence/kysely/repositories/read/helpers/get-inbox-by-user-id.ts
@@ -12,8 +12,6 @@ async function getInboxByUserId(
     .qb(trx)
     .selectFrom('groups as g')
     .leftJoin('task_to_group as ttg', 'ttg.group_id', 'g.id')
-    .leftJoin('tasks as t', 't.id', 'ttg.task_id')
-    .leftJoin('task_statuses as ts', 'ts.id', 't.status_id')
     .where('g.name', '=', INBOX_GROUP_KEY)
     .where('g.user_id', '=', input.userId)
     .select(['g.id as id', 'g.user_id as user_id', 'g.name as name'])
