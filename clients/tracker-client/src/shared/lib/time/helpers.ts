619b0a7d87b438fb344f4dccd2c901d00ed95193
Dean Korobchenko <deankorobchenko@gmail.com>
2026-01-31 23:45:53 +0700

    fix: helper

--- clients/tracker-client/src/shared/lib/time/helpers.ts
+++ clients/tracker-client/src/shared/lib/time/helpers.ts
@@ -1,17 +1,17 @@
 import type { ConfigType, Dayjs } from './';
 import dayjs from './';
 
-function getClosestTimeToNow(dates: (string | undefined | Dayjs)[]): ConfigType | undefined {
+function getClosestTimeToNow(dates: (string | undefined | Dayjs)[]): ConfigType {
   if (dates.length === 0) return;
 
   const now = dayjs().millisecond(1);
-  const onlyBeforeNowDates = dates
-    .filter((date) => dayjs(date).isSameOrAfter(now))
+  const onlySameOrBeforeNowDates: Dayjs[] = dates
+    .filter(Boolean)
     .map((date) => dayjs(date))
-    .filter((date) => date.millisecond() >= now.millisecond());
-  if (onlyBeforeNowDates.length === 0) return;
+    .filter((date) => date.isSameOrAfter(now));
+  if (onlySameOrBeforeNowDates.length === 0) return;
 
-  return dayjs.min(onlyBeforeNowDates);
+  return dayjs.min(onlySameOrBeforeNowDates);
 }
 
 export { getClosestTimeToNow };
