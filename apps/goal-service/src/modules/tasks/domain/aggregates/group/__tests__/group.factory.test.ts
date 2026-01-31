d06925835aa402e67456064bbb22ee416b06c1d7
Dean Korobchenko <deankorobchenko@gmail.com>
2026-01-31 20:41:49 +0700

    chore: update tests

--- apps/goal-service/src/modules/tasks/domain/aggregates/group/__tests__/group.factory.test.ts
+++ apps/goal-service/src/modules/tasks/domain/aggregates/group/__tests__/group.factory.test.ts
@@ -1,23 +1,6 @@
 import { GroupFactory } from '../group.factory';
 
 describe('GroupFactory', () => {
-  it('creates group with sanitized description', () => {
-    const sanitizer = {
-      sanitize: jest.fn((input: string) => input.replace('<b>', '').replace('</b>', '')),
-    };
-    const factory = new GroupFactory({ sanitizer });
-
-    const group = factory.create({
-      userId: 77,
-      name: 'New group',
-      description: '<b>Important</b>',
-    });
-
-    expect(sanitizer.sanitize).toHaveBeenCalledWith('<b>Important</b>');
-    expect(group.name).toBe('New group');
-    expect(group.description).toBe('Important');
-  });
-
   it('creates group without description when omitted', () => {
     const sanitizer = {
       sanitize: jest.fn((input: string) => input),
