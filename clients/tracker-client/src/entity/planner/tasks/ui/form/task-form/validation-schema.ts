1fda7adf8c6e63d0432c9276f52fe7f384ee546c
Dean Korobchenko <deankorobchenko@gmail.com>
2026-02-02 21:28:28 +0700

    revirefix: validation-schema.ts issue

--- clients/tracker-client/src/entity/planner/tasks/ui/form/task-form/validation-schema.ts
+++ clients/tracker-client/src/entity/planner/tasks/ui/form/task-form/validation-schema.ts
@@ -2,39 +2,54 @@ import { formPlaceholderValues, formTransform, transformPlaceholder } from '@/sh
 import { z } from 'zod';
 import { taskPrioritySchema } from '../../../lib/validation-schemas';
 
-const validationSchema = z.object({
-  name: z
-    .string({ error: '' })
-    .min(3, { error: 'Не меньше 3 символов' })
-    .max(254, { error: 'Слишком длинное имя' }),
+const validationSchema = z
+  .object({
+    name: z
+      .string({ error: '' })
+      .min(3, { error: 'Не меньше 3 символов' })
+      .max(254, { error: 'Слишком длинное имя' }),
 
-  priority: z
-    .enum(taskPrioritySchema)
-    .optional()
-    .or(z.literal(null))
-    .transform(transformPlaceholder.optional),
+    priority: z
+      .enum(taskPrioritySchema)
+      .optional()
+      .or(z.literal(null))
+      .transform(transformPlaceholder.optional),
 
-  description: z.string().optional().transform(transformPlaceholder.optional),
-  isDescriptionDirty: z.boolean(),
+    description: z.string().optional().transform(transformPlaceholder.optional),
+    isDescriptionDirty: z.boolean(),
 
-  weight: z
-    .number({ error: '' })
-    .max(100, { error: 'Не больше 100' })
-    .positive({ error: 'Только больше 0' })
-    .transform(transformPlaceholder.percentNumber),
+    weight: z
+      .number({ error: '' })
+      .max(100, { error: 'Не больше 100' })
+      .positive({ error: 'Только больше 0' })
+      .transform(transformPlaceholder.percentNumber),
 
-  startDate: z
-    .date()
-    .optional()
-    .or(z.literal(formPlaceholderValues.date))
-    .transform(formTransform.dateToISOSFormat),
+    startDate: z
+      .date()
+      .optional()
+      .or(z.literal(formPlaceholderValues.date))
+      .transform(formTransform.dateToISOSFormat),
 
-  deadline: z
-    .date()
-    .optional()
-    .or(z.literal(formPlaceholderValues.date))
-    .transform(formTransform.dateToISOSFormat),
-});
+    deadline: z
+      .date()
+      .optional()
+      .or(z.literal(formPlaceholderValues.date))
+      .transform(formTransform.dateToISOSFormat),
+  })
+  .check((ctx) => {
+    const { startDate, deadline } = ctx.value;
+
+    if (deadline != null && startDate != null) {
+      if (startDate >= deadline) {
+        ctx.issues.push({
+          path: ['deadline'],
+          input: ctx.value.deadline,
+          code: 'custom',
+          message: 'Дедлайн не может быть позже или равен началу',
+        });
+      }
+    }
+  });
 
 type TaskFormData = z.input<typeof validationSchema>;
 type TaskSubmitFormData = z.output<typeof validationSchema>;
