90c9ccf3b80f00939e5403253ce8ccee3a37f165
Dean Korobchenko <deankorobchenko@gmail.com>
2026-02-24 11:44:12 +0700

    fix: group deleting invalidation

--- clients/tracker-client/src/entity/planner/groups/model/state/use-group-delete.ts
+++ clients/tracker-client/src/entity/planner/groups/model/state/use-group-delete.ts
@@ -1,12 +1,24 @@
 import { $privetQueryClient } from '@/shared/api/api-client';
+import { queryClient } from '@/shared/api/query-client';
 import { getDefaultQueryNotifications } from '@/shared/lib/react/default-notifications';
+import { groupsQueryKeys } from './query';
 
 function useGroupDelete() {
   const options = getDefaultQueryNotifications();
   const { mutate: deleteGroup, ...others } = $privetQueryClient.useMutation(
     'delete',
     '/groups/{groupId}',
-    { onError: options.onError },
+    {
+      onError: options.onError,
+      onSuccess: async (_, { params }) => {
+        queryClient.setQueriesData(
+          {
+            queryKey: groupsQueryKeys.getGroupById({ groupId: params.path.groupId }),
+          },
+          null,
+        );
+      },
+    },
   );
 
   return {
