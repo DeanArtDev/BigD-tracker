e9cbbcfa43c46f0b6678f37a0f664023a90bce6e
Дмитрий <deanartdev@gmail.com>
2026-05-29 14:19:02 +0000

    Feat/planner sidebar

--- clients/web-client/src/app/(public)/login/page.tsx
+++ clients/web-client/src/app/(public)/login/page.tsx
@@ -3,8 +3,8 @@ import { Main } from '../../_ui/main';
 
 export default function LoginPage() {
   return (
-    <Main>
-      <div className="flex mx-auto w-full max-w-[1440px] items-center">
+    <Main className="min-h-dvh min-w-dvw">
+      <div className="flex mx-auto w-full h-full max-w-[1440px] items-center">
         <div className="flex-1/2">
           <div className="bg-linear-to-bl grow from-indigo-400 to-violet-800 mb-auto mr-auto rounded-xl h-[760px] w-[560px]"></div>
         </div>
